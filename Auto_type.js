a23fc4f2e2d5053533336ee2773bba2cd9ace6be
Sambhram Matahd <125111476+SambhramMathad13@users.noreply.github.com>
2023-02-12 22:23:53 -1000

    Add files via upload

---
+++ Auto_type.js
@@ -0,0 +1,23 @@
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+
+let str="Hi I am Sambhram Mathad...   "
+var typed = new Typed(".at", {
+    strings: [str],
+    typeSpeed: 70,
+    backSpeed: 100,
+    // loop: true
+    
+})
+var typed = new Typed(".na", {
+    strings: ["Lost in GITHUB...(13-02-2023)"],
+    typeSpeed: 70,
+    backSpeed: 100,
+    // loop: true
+    
+})
+var typed = new Typed(".n", {
+    strings: [" love coding..."," love playing footbal..."," love listning music..."],
+    typeSpeed: 80,
+    backSpeed: 80,
+    loop: true
+})
